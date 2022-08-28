3a8bf90d67a650c3ebf7a56ab52975fac94a069c
@mtorres <tcontreras993@gmail.com>
2022-08-28 13:11:15 -0300

    primer comit con pagina agregada

--- next.config.js
+++ next.config.js
@@ -1,7 +1,67 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const sites = {
+  site1: {
+    locales: {
+      ar: {
+        title: "Adidas",
+        link: [
+          "https://www.adidas.com.ar/camiseta-titular-argentina-22-messi/HL8425.html",
+          "https://www.adidas.com.ar/camiseta-titular-river-plate-22-23/GB7592.html",
+        ],
+      },
+    },
+    domain: "localhost1",
+  },
+  site2: {
+    locales: {
+      ar: {
+        title: "Nike",
+        link: [
+          "https://www.dexter.com.ar/camiseta-nike-atletico-madrid-2022%2F23-std-home/NI_DM1838-101.html?utm_source=display&utm_medium=nike&utm_campaign=EMEA&utm_campaign=nikeutm-om-atletico-de-madrid&utm_term=22-23&utm_source=nike&utm_medium=web-pdp&utm_content=comprar-2",
+          "https://www.dexter.com.ar/camiseta-nike-liverpool-fc-2022%2F23-stadium-home/NI_DM1843-609.html?utm_source=display&utm_medium=nike&utm_campaign=EMEA&utm_campaign=nikeutm-om-liverpool-22-23&utm_source=nike&utm_medium=web-pdp&utm_content=comprar-1",
+        ],
+      },
+    },
+    domain: "localhost2",
+  },
+};
 
-module.exports = nextConfig
+const siteKeys = Object.keys(sites)
+
+const pseudoLocales = ['ar']
+
+
+const nextConfig = {
+  publicRuntimeConfig: {
+    sites,
+    pseudoLocales
+  },
+
+  i18n: {
+    locales: siteKeys,
+    defaultLocale: siteKeys[0],
+    domain: siteKeys.map(siteKey => (
+      {
+        domain: sites[siteKey].domain,
+        defaultLocale: siteKeys
+      }
+    ))
+  },
+
+  redirects: () => (
+    [
+      {
+        source: '/',
+        destination: `/${pseudoLocales[0]}`,
+        permanent: true,
+      }
+    ]
+  ),
+
+  future: {
+    webpack5: true,
+  }
+
+};
+
+module.exports = nextConfig;
---
+++ pages/[pseudoLocale]/index.js
@@ -0,0 +1,74 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import getConfig from "next/config";
+
+export async function getStaticPaths({ locales }) {
+  return {
+    paths: [{ params: { pseudoLocale: "ar" }, locale: "site1" }],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({
+  params: { pseudoLocale = "ar" },
+  locale = "site1",
+}) {
+  return {
+    props: {
+      pseudoLocale,
+    },
+    revalidate: 60,
+  };
+}
+
+export default function Home({ pseudoLocale }) {
+  const { locales, locale } = useRouter();
+  const {
+    publicRuntimeConfig: { sites, pseudoLocales },
+  } = getConfig();
+
+  const nextLocale = pseudoLocales.find(
+    (otherLocale) => otherLocale !== pseudoLocale
+  );
+  const nextSiteNr = locale === "site2" ? 1 : 2;
+  console.log(nextSiteNr);
+
+  const siteAndLocale = sites[locale] && sites[locale].locales[pseudoLocale];
+
+  console.log(siteAndLocale);
+
+  return (
+    <>
+      <Head>
+        <title>Multi dominio Prueba</title>
+      </Head>
+
+      <main>
+        <h1>
+          {siteAndLocale
+            ? siteAndLocale.title
+            : `el sitio ${locale} no funciona`}{" "}
+        </h1>
+        {siteAndLocale && (
+          <ul>
+            {siteAndLocale.link.map((link) => (
+              <li key={link}>
+                <Link href={link}>
+                  <a>{link}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+
+        <div>
+          <a href={`http://localhost${nextSiteNr}:3000/site${nextSiteNr}/${pseudoLocale}`}>
+            <p>estas en el citio {locale}</p>
+            <p>vas ir al sitio {nextSiteNr} </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
